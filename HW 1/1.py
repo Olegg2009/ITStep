3359416860f886bce887f54ffad38ae1465869b7
Oleg <olegbytkov@gmail.com>
2021-12-31 22:11:15 +0300

    Home work add

---
+++ HW 1/1.py
@@ -0,0 +1,11 @@
+Name = input('Введите имя: ')
+Age = input('Введите возраст: ')
+PhoneNumber = input('Введите номер телефона:')
+
+# int(input()) - ввод чисел (int)
+# input() - ввод символов (str)
+
+print("Ваши данные: ")
+print(f'Имя: {Name}')
+print(f'Возраст: {Age}')
+print(f'Номер телефона: {PhoneNumber}')
